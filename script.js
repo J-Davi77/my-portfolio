26b1976478412037871988f2e8a0a7eed13ca8dc
Júlio Davi <julio1704davi@gmail.com>
2025-10-25 21:20:51 -0300

    Adiciona um glow nos cards de habilidades

--- script.js
+++ script.js
@@ -12,30 +12,34 @@ document.addEventListener("DOMContentLoaded", () => {
     );
 });
 
+// Glow nos cards
+const cards = document.querySelectorAll(".card");
+function glowCard(e, card) {
+    const glowDiv = card.querySelector(".glow");
+    if (glowDiv) {
+        const rect = card.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+        glowDiv.style.left = `${x}px`;
+        glowDiv.style.top = `${y}px`;
+        glowDiv.style.opacity = 1;
+    }
+}
+
+cards.forEach((card) => {
+    card.addEventListener("mousemove",(e) => glowCard(e ,card));
+
+    card.addEventListener("mouseleave", () => {
+        const glowDiv = card.querySelector(".glow");
+        if (glowDiv) glowDiv.style.opacity = 0;
+    });
+});
+
 // Menu para telas menores
 const menuBtn = document.querySelector("#menu-btn");
+const header = document.querySelector("header");
 function toggleMenu() {
-    const header = document.querySelector("header");
     header.classList.toggle("show-menu");
-    if (header.classList.contains("show-menu")) {
-        menuBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="25" height="30" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
-            <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
-        </svg>`;
-    } else {
-        menuBtn.innerHTML = `<svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="30"
-                        height="30"
-                        fill="currentColor"
-                        class="bi bi-list"
-                        viewBox="0 0 16 16"
-                        >
-                        <path
-                        fill-rule="evenodd"
-                        d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
-                        />
-                        </svg>`;
-    }
 }
 menuBtn.addEventListener("click", toggleMenu);
 
@@ -64,8 +68,6 @@ modalOverlay.addEventListener("click", (e) => {
 });
 
 // Animação de scroll
-const cards = document.querySelectorAll(".card");
-
 const observer = new IntersectionObserver(
     (entries) => {
         entries.forEach((entry) => {
@@ -91,10 +93,10 @@ function setInitialTheme() {
 
     if (prefersDark.matches) {
         document.body.setAttribute("data-bs-theme", "dark");
-        darkmodeBtn.classList.add('darkmode')
+        darkmodeBtn.classList.add("darkmode");
     } else {
         document.body.setAttribute("data-bs-theme", "light");
-        darkmodeBtn.classList.remove('darkmode')
+        darkmodeBtn.classList.remove("darkmode");
     }
 }
 
